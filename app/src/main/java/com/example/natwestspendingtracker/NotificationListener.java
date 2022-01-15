6f94b7f84146fce6a6ca003935063ecce59adefd
Alex Constantinou <a_constantinou@hotmail.co.uk>
2022-01-15 12:54:43 +0000

    price extraction regex working

--- app/src/main/java/com/example/natwestspendingtracker/NotificationListener.java
+++ app/src/main/java/com/example/natwestspendingtracker/NotificationListener.java
@@ -7,10 +7,14 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NotificationListener extends NotificationListenerService {
 
     private String TAG = "Notification Received";
+    private String NATWEST_NOTIFICATION = "natwest";
     private ArrayList<String> notifications = new ArrayList<>();
 
     @Override
@@ -48,18 +52,30 @@ public class NotificationListener extends NotificationListenerService {
     private void prepareNotification(StatusBarNotification sbn) {
         int id = sbn.getId();
         String name = sbn.getPackageName();
-//        long time = sbn.getPostTime();
-//        boolean clearable = sbn.isClearable();
-//        boolean playing = sbn.isOngoing();
-//        CharSequence text = sbn.getNotification().tickerText;
-        String text = sbn.getNotification().extras.get(Notification.EXTRA_BIG_TEXT).toString();
+        String text = "None";
+        if (sbn.getNotification().extras.containsKey(Notification.EXTRA_BIG_TEXT)) {
+            text = sbn.getNotification().extras.get(Notification.EXTRA_BIG_TEXT).toString();
+
+        }
+        String price = "0.00";
+        if(sbn.getPackageName().contains(NATWEST_NOTIFICATION)){
+            price = extract_cost_dot(text).toString();
+        }
 
         String notification = TAG + "\n" +
                 "id: " + id + "\n" +
                 "name: " + name + "\n" +
-                "text: " + text;
+                "text: " + text + "\n" +
+                "price: " + price;
         Intent intent = new  Intent("com.example.natwestspendingtracker");
         intent.putExtra("Notification", notification);
         sendBroadcast(intent);
+    }
+
+    public   Double extract_cost_dot(String cost) {
+        Pattern compile = Pattern.compile("(\\d+\\.\\d+)|(\\d+)");
+        Matcher matcher = compile.matcher(cost);
+        matcher.find();
+        return Double.valueOf(matcher.group());
     }
 }
